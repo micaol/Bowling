40e928129fcde5c7fde1919c187066b6f6a39316
micaol <micael.chaudy@gmail.com>
2018-08-08 22:47:35 +0100

    Add xunit test project (#2)

---
+++ Bowling.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using Bowling.Controllers;
+
+namespace Bowling.Tests
+{
+    public class BowlingUnitTest
+    {
+        private readonly ValuesController _myController;
+
+        public BowlingUnitTest()
+        {
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
